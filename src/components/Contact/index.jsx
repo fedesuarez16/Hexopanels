6416fdefba4a13c280d8bd6fb7e3e27109ecd783
fedesuarez16 <fedesuarez16@gmail.com>
2024-01-02 18:20:30 -0300

    deployment

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,7 +1,6 @@
 // components/Contact.js
 import React from 'react';
 import styles from './style.module.css';
-import WhatsAppButton from './WhatsAppButton';
 
 const Contact = () => {
   const phoneNumber = '123456789'; // Replace with your phone number
@@ -11,7 +10,6 @@ const Contact = () => {
     <div className={styles.contactContainer}>
       <h2 className={styles.contactHeading}>Contact Us</h2>
       <p className={styles.contactText}>Feel free to contact us via WhatsApp.</p>
-      <WhatsAppButton phoneNumber={phoneNumber} message={defaultMessage} className={styles.whatsappButton} />
     </div>
   );
 };
